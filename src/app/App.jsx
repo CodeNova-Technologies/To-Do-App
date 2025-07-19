40e1b6fd9a4143883a7f7cc5f4f1d2f6784dd662
Geethanga <dissanayakegeethanga@gmail.com>
2025-07-19 16:51:23 +0530

    Improve App.jsx Code

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import CompletedTasksPage from './pages/Task/CompletedTasksPage';
+import CompletedTasksPage from '../pages/Task/CompletedTasksPage';
 
 function App() {
   return (
